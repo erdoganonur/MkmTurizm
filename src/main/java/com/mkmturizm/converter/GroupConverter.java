45b338706f16b751a0c4cb529dd09cc5db4f2287
erdoganonur <onurerdogan@Onur-MacBook-Pro.local>
2013-10-19 18:50:22 +0300

    ccc

--- src/main/java/com/mkmturizm/converter/GroupConverter.java
+++ src/main/java/com/mkmturizm/converter/GroupConverter.java
@@ -11,8 +11,6 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
-import javax.faces.convert.FacesConverter;
-import javax.inject.Inject;
 
 /**
  *
